1956788006a45dfdfac56a83025c9c428f49dc0e
Nilo <nilo.cara@gmail.com>
2023-06-12 10:19:41 +0200

    Readme.txt

--- finance_calculators.py
+++ finance_calculators.py
@@ -1,41 +1,41 @@
 #SEL1T11 Capstone Project 1 - Variables and Control Structures
 
-#Import Math Library
 import math
 
-#r = (rate_interest / 100 ) / 12
-# User select and input
-print("\nPlease choose your type financial service: 'Investment' or 'bond';\n\nInvestment - to calculate the amount of intrest you will earn on your investment \nBond - to calculate the amount you will have to pay on a home loan\n")
-#user to select a type of product: 
-select_service = str(input("Please enter: \nInvestment or Bond: ")).capitalize()
+print("\nPlease choose your type of financial service: 'Investment' or 'Bond';\n\nInvestment - to calculate the amount of interest you will earn on your investment\nBond - to calculate the amount you will have to pay on a home loan\n")
 
-# Investment option
+select_service = input("Please enter: Investment or Bond: ").capitalize()
+
 if select_service == "Investment":
-    if True:
-        principal_invest = float(input("Please enter the your principal amount you wish to invest: \n"))
-        rate_interest = float(input("Please enter the interest rate amount: \n"))
-        r = (rate_interest / 100 ) / 12
-        time_invest = float(input("Please enter the total number of years you wish to invest for e.g 5, 10, 15, 20: "))
-    interest = str(input("Please enter: \nSimple or Compound: ")).capitalize()
-# if statement condition based on the simple and compound calcutations 
-    if interest == "Simple": 
+    principal_invest = float(input("Please enter the principal amount you wish to invest: "))
+    rate_interest = float(input("Please enter the interest rate amount: "))
+    time_invest = float(input("Please enter the total number of years you wish to invest for (e.g., 5, 10, 15, 20): "))
+
+    interest = input("Please enter: Simple or Compound: ").capitalize()
+
+    if interest == "Simple":
+        r = (rate_interest / 100) / 12
         interest = principal_invest * (1 + r * time_invest)
-        print("Your interest that will be earn over %0.2f" %time_invest, "years will be R%0.2f" %interest)
+        print("Your interest that will be earned over %.2f years will be R%.2f" % (time_invest, interest))
         total = principal_invest + interest
-        print("Your total payout after %0.0f" %time_invest, " years will be R%0.2f" %total)  
-    elif interest == "Compound": 
-        interest = principal_invest * math.pow(( 1 + r ), time_invest)
-        print("Your interest that will be earned over %0.2f" %time_invest, "years will be R%0.2f" %interest)
+        print("Your total payout after %.0f years will be R%.2f" % (time_invest, total))
+    elif interest == "Compound":
+        r = (rate_interest / 100) / 12
+        interest = principal_invest * math.pow((1 + r), time_invest)
+        print("Your interest that will be earned over %.2f years will be R%.2f" % (time_invest, interest))
         total = principal_invest + interest
-        print("Your total payout after %0.0f" %time_invest, "years will be R%0.2f" %total)
-# if statement to calculate the bond option
+        print("Your total payout after %.0f years will be R%.2f" % (time_invest, total))
+    else:
+        print("Invalid input for interest type. Please choose either Simple or Compound.")
+
 elif select_service == "Bond":
-    if True:
-        bond_house_val = float(input("Please enter your home's current market value: \n"))
-        bond_rate_interest = float(input("Please enter the interest rate amount: \n"))
-        i = (bond_rate_interest / 100 ) / 12
-        bond_term = float(input("Please enter the bond term in months e.g \n10 years = 120 months, \n15 years = 180 months, \n20 years = 240 months, \n30 years = 360 months: \nMonths: "))
-        repay = float(math.floor( i * bond_house_val) / (1 - (1 + i ) ** (- bond_term)))
-        print("Your monthly repayment will be: R%0.2f" %repay, "for %0.0f months" %bond_term, "at %0.2f percent" %bond_rate_interest )
+    bond_house_val = float(input("Please enter your home's current market value: "))
+    bond_rate_interest = float(input("Please enter the interest rate amount: "))
+    bond_term = float(input("Please enter the bond term in months (e.g., 10 years = 120 months, 15 years = 180 months, 20 years = 240 months, 30 years = 360 months): "))
+
+    i = (bond_rate_interest / 100) / 12
+    repay = math.floor(i * bond_house_val) / (1 - math.pow((1 + i), -bond_term))
+    print("Your monthly repayment will be: R%.2f for %.0f months at %.2f percent" % (repay, bond_term, bond_rate_interest))
+
 else:
-    print("Oh No! Let's try it again")
+    print("Invalid input for financial service type. Please choose either Investment or Bond.")
---
+++ readme.txt
@@ -0,0 +1,2 @@
+### My program is a financial service calculator that calculates either the interest on an investment or the monthly repayment for a home loan (bond).
+
